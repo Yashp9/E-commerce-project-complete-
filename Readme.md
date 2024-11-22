f71c8f649aa7d182a078a1d02b1c62287287114f
Yashp9 <pandeyyash058@gmail.com>
2024-11-22 23:39:40 +0530

    lerning-4

--- Readme.md
+++ Readme.md
@@ -7,3 +7,7 @@ learn how to use github using vscode
 -->Learning -3 (0 to 27:00)
 -create mongodb connection inside lib>db.js and connected to server
 -created userModel inside model>user.model.js
+
+-->Learning -4 (27 to 31)
+-create static function to generate hash password.
+-->also create a logic to compare the password.
--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -25,17 +25,57 @@ const userSchema = new mongoose(
                 default:1,
             },
             product:{
+
+                //Indicates that product will store a reference to another document in the database.
                 type:mongoose.Schema.Types.ObjectId,
-                ref:"Product",//capital P
+
+                //When you use ref: "Product", you are telling Mongoose that the product field in your schema is connected to another collection in the database called "Product". Think of it as a way to link two types of data together.
+                ref:"Product",
             }
         }],
         role:{
-            typr:String,
+            type:String,
+
+            //enum ensures that the role can only be "cutomer" or "admin".
             enum:["customer","admin"],
+            //by default it will be customer.
             default:"customer",
         },
     },
     {
         timestamps:true,
     }
-)
+);
+
+// Mongoose pre-save middleware for a userSchema. This middleware runs before saving a user document in the database.
+//Here, the next function is used in the context of Mongoose middleware, which is a bit different from Express middleware, though the naming convention is the same.
+
+// Mongoose Middleware: The next function in Mongoose is used to control the flow of execution in hooks like pre or post on a model. When you call next(), you’re telling Mongoose to continue with the operation (like saving a document).
+
+userSchema.pre('save',async function (next) {
+
+    //this.isModified(field) is a Mongoose method that checks whether the specified field (in this case, "password") has been changed in the current operation. accordingly it returns true or false.
+    if(!this.isModified('password')) return next();
+
+    try{
+        //genrating hashed passwords.
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password,salt);
+        next();
+    }catch(error){
+
+        //passing error to the next middleware
+        next(error);
+    }
+});
+
+//creating static method which called by the instance of this useschema model like form user1 or user2
+userSchema.methods.comparePassword = async function(password){
+
+    //here the this keyword refers to the user1 or user2 whoever called this methods->user1.comparePassword.
+    return bcrypt.compare(password,this.password);
+}
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
